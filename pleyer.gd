f2d5ea5608ff072da7867b672da8516e674e71b4
steven3030 <stav.weiss.t@gmail.com>
2022-03-10 18:41:24 +0200

    changes by stav

--- pleyer.gd
+++ pleyer.gd
@@ -1,5 +1,13 @@
 extends KinematicBody
 
+onready var camera = $pivot/camera
+
+var gravity = - 30
+var max_speed = 8
+var mouse_sensitivity = 0.002   #radians
+
+
+
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -25,3 +33,6 @@ func _process(delta):
 	if Input.is_action_pressed("move_forward"):
 		vect.z -= 100
 	move_and_slide(vect)
+
+
+	
