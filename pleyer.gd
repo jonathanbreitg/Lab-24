17db43d758cec9055b1bdba4e614fe143aef6cb1
britgand <jonathan.breitgand@gmail.com>
2022-03-10 19:38:56 +0200

    game is now first person, jumping works, floor clipping bug fixed, and other major changes

--- pleyer.gd
+++ pleyer.gd
@@ -1,12 +1,15 @@
 extends KinematicBody
+export var G = 1.5
+var speed_y = 0
+var up_dir = Vector3.UP
+var is_jumping = true
+export var movement_speed = 200
+export var jump_impulse = 400
 
-onready var camera = $pivot/camera
-
+#@stav you never use these, why did you just define variables without doing anything with them?????
 var gravity = - 30
 var max_speed = 8
 var mouse_sensitivity = 0.002   #radians
-
-
 
 
 # Declare member variables here. Examples:
@@ -17,22 +20,34 @@ var vect : Vector3 = Vector3.ZERO
 func _ready():
 	pass # Replace with function body.
 	
-
-
 func _process(delta):
-	print(delta)
-	vect = Vector3.ZERO
-
+	vect.x = 0
+	vect.z = 0
 	# We check for each move input and update the vect accordingly.
 	if Input.is_action_pressed("move_right"):
-		vect.x += 100
+		vect.x -= movement_speed
 	if Input.is_action_pressed("move_left"):
-		vect.x -= 100
+		vect.x += movement_speed
 	if Input.is_action_pressed("move_back"):
-		vect.z += 100
+		vect.z -= movement_speed
 	if Input.is_action_pressed("move_forward"):
-		vect.z -= 100
-	move_and_slide(vect)
+		vect.z += movement_speed
+	if Input.is_action_just_pressed("jump"):
+		vect.y = vect.y + jump_impulse
+		is_jumping = true
+		print("jump")
+	move_and_slide(vect,up_dir)
+	print('is jumping is',is_jumping)
+	print('speed_y is ',speed_y)
+	print('vect.y is ', vect.y)
+	print('on_floor is ',is_on_floor())
+	if is_on_floor():
+		print('')
+		if !is_jumping:
+			speed_y = 0
+			vect.y = 0
 
-
-	
+	else:
+		is_jumping = false
+		speed_y = speed_y + G
+	vect.y = vect.y - speed_y
