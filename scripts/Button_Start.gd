abe58f6a6eb33b2c89308f27bf7647ddbfb735e2
britgand <jonathan.breitgand@gmail.com>
2022-03-17 20:20:59 +0200

    SO MANY CHANGES AAAAAA

--- scripts/Button_Start.gd
+++ scripts/Button_Start.gd
@@ -4,16 +4,22 @@ extends Button
 # var a = 2
 # var b = "text"
 var level = "level1"
+var exists
 var s
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var file = File.new()
-	file.open("user://cur_level.dat", File.READ)
-	var content = file.get_as_text()
-	file.close()
-	level = str(content)
-	level = level.replace("\n","")
-	print(level)
+	exists = file.file_exists("user://cur_level.dat")
+	if exists:
+		file.open("user://cur_level.dat", File.READ)
+		var content = file.get_as_text()
+		file.close()
+		level = str(content)
+		level = level.replace("\n","")
+		print(level)
+	else:
+		level = "level1"
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
